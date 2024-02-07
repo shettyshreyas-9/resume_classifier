ffd3459ecb857b993090ce4f8b3ce9def389ac27
shettyshreyas-9 <shreyasshetty925@gmail.com>
2024-02-07 23:56:25 +0530

    created make_dataset.py file are completed load data section

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,65 @@
-# -*- coding: utf-8 -*-
-import click
+import pathlib
+import os
+import sys
 import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import yaml
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+
+def setup_logging():
+    log_file_path = pathlib.Path(__file__).parent.as_posix()+ sys.argv[3]
+    logging.basicConfig(
+        filename=log_file_path,
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s]: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+def load_data(data_path):
+    df = pd.read_csv(data_path,encoding='utf-8')
+    return df
+
+
+def save_data(data_df,output_path):
+    data_df.to_csv(output_path+'/processed_data.csv')
+
+    logging.info("Saved data successfully")
+
+
+
+
+def main():
+
+    # calling the logging function
+    setup_logging()
+
+    # project setup via cookiecutter may give problem using (__file__) so used (os.getcwd())
+    curr_dir = pathlib.Path(__file__)
+    working_dir= pathlib.Path(os.getcwd())
+    home_dir = curr_dir.parent.parent.parent
+
+    # input data
+    input_file= sys.argv[1]
+    data_path= working_dir.as_posix() + input_file
+
+    # print ('\n',data_path,'\n')
+
+    data_df= load_data(data_path)
+
+    logging.info("Loaded data successfully")
+
+    # Save data
+    output_path= working_dir.as_posix() + sys.argv[2]
+    save_data(data_df=data_df,output_path=output_path)
+
+
+
+
 
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
     main()
